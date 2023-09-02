9ad1eda473314b3dd21ef39c3e17163ec71e7689
Dibyendu Das <get2dibyendu@gmail.com>
2023-09-02 06:13:45 +0530

    payment section added

---
+++ src/components/PaymentBackground.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import bgImage from '../images/payments/Vector.png'
+import column1 from '../images/payments/payments-column.png'
+import column3 from '../images/payments/payments-column (2).png'
+import pillarLarge from '../images/payments/payments-pillar-large.png'
+import pillarMedium from '../images/payments/payments-pillar-medium.png'
+import pillarSmall from '../images/payments/payments-pillar-small.png'
+
+
+const PaymentBackground = () => {
+  return (
+    <div className="overflow-hidden ">
+        <img src={bgImage} className="w-full absolute bottom-[0%]"/>
+
+        <img src={column1} className="w-[810px] absolute right-0 bottom-[10%]"/>
+        <img src={column3} className="w-[810px] z-10 absolute left-[28%] bottom-[0]"/>
+        <img src={column3} className="w-[810px] z-10 absolute left-[-40%] bottom-[0]"/>
+
+        <img src={pillarLarge} className="w-[210px] absolute left-[48%] bottom-[10%]"/>
+        <img src={pillarMedium} className="w-[172px] absolute right-[16%] bottom-[25%]"/>
+        <img src={pillarMedium} className="w-[172px] absolute left-[48%] bottom-[-8%]"/>
+        <img src={pillarMedium} className="w-[172px] z-10 absolute left-[40%] bottom-[10%]"/>
+        <img src={pillarMedium} className="w-[172px] z-10 absolute left-[15%] bottom-[8%]"/>
+        <img src={pillarSmall} className="w-[135px] z-5 absolute left-[22%] bottom-[15%]"/>
+        
+        
+
+
+        
+    </div>
+  )
+}
+
+export default PaymentBackground
--- src/sections/Payment.js
+++ src/sections/Payment.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import phone from "../images/payments/payments-phone 1.png"
+import PaymentBackground from "../components/PaymentBackground";
 
 const Payment = () => {
   return (
-    <div className="h-screen">
-        Payment
-    </div>
-  )
-}
+    <div
+    // style={{ backgroundImage: `url(${bgImage})`,  backgroundSize: "cover"}}
+    className="h-[105vh] relative flex items-center">
+        
+        <PaymentBackground/>
 
-export default Payment
+      <div className="w-80 ml-80 mb-40 font-agrandir flex flex-col gap-4">
+        <h1 className="w-[336px] text-green-500 text-[40px] font-extrabold leading-10 ">Payments</h1>
+        <p className="text-sm font-normal leading-snug tracking-tight">
+          Send and receive money with anyone, donate to an important cause, or
+          tip professionals. Just enter a $cashtag, phone number, or scan their
+          QR code to pay.
+        </p>
+        <img src={phone} alt="phone" width="750px" className="absolute left-[25%] bottom-[30%] "/>
+      </div>
+    </div>
+  );
+};
+
+export default Payment;
